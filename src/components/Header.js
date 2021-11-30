cf2f5aead00bcbf80c59c95fed01d3894af544c9
Burak <sarisahinburak22@gmail.com>
2021-11-30 13:30:56 +0100

    re

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+function header() { //once function yaziyoruz
+    return(
+        <header>
+            <h1>Title Header</h1>
+        </header> 
+    )
+}
+
+export default header; //sonra export ediyoruz // main klasorunde exportu uste yazdik
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <h3>User App</h3>
+        </div>
+    )
+}
